0921bbf09d2320f85effe2939f14690a5f4e11db
Leanderson Ficagna <64813850+LeanFicagna@users.noreply.github.com>
2022-03-24 11:53:06 -0300

    Update Constants.ts

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,5 +1,5 @@
 export const config = {
     API_URL: 'http://localhost:5000',
     MONGO_CONNECTION:
-    'mongodb+srv://root:xLSzGytoXBLr8UqZ@url-shortener-dio.yfbwb.mongodb.net/url-shortener-dio?retryWrites=true&w=majority'
+    '<url>'
 }
